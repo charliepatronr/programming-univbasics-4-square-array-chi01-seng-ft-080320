4b09f6fc4ba7f4605f5401c7a80b010c5a0b41ed
Juan Carlos Patron <charliepatronr@gmail.com>
2020-07-12 21:31:22 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   length = array.length 
   new_arr =[]
   
-  while length.times do |index|
+  length.times do |index|
     new_arr.push(Math.sqrt(array[index]))
   end
   new_arr
